a66e3bd289f4289184cae74a0802ea728539a0be
MM <michele.makiki@gmail.com>
2023-12-14 13:13:56 +0200

    helper functions and unit tests step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,10 +1,31 @@
+/* eslint-disable no-console */
 const dummy = ((blogs) => 1);
 
 const totalLikes = (blogs) => (blogs.lenght === 0
   ? 0
   : blogs.reduce((sum, item) => sum + item.likes, 0));
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length > 0) {
+    const blogWithMostLikes = (blogs.reduce(
+      (previous, current) => (previous.likes > current.likes
+        ? previous
+        : current),
+    )
+    );
+    const blogWithMostLikesResult = {
+      title: blogWithMostLikes.title,
+      author: blogWithMostLikes.author,
+      likes: blogWithMostLikes.likes,
+    };
+    console.log(blogWithMostLikesResult);
+    return blogWithMostLikesResult;
+  }
+  return {};
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
