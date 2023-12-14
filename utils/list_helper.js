1e750a4fdbda9644c97fe4619701ff5a8e8f06d4
MM <michele.makiki@gmail.com>
2023-12-14 12:49:52 +0200

    helper functions and unit tests step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,10 @@
 const dummy = ((blogs) => 1);
 
+const totalLikes = (blogs) => (blogs.lenght === 0
+  ? 0
+  : blogs.reduce((sum, item) => sum + item.likes, 0));
+
 module.exports = {
   dummy,
+  totalLikes,
 };
