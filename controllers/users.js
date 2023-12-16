3547c6bf048f5e99867c02655da484070bd7e1d6
MM <michele.makiki@gmail.com>
2023-12-16 13:25:59 +0200

    bloglist expansion, step5

--- controllers/users.js
+++ controllers/users.js
@@ -4,12 +4,17 @@ const User = require('../models/user');
 const logger = require('../utils/logger');
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({});
+  const users = await User
+    .find({}).populate('blogs', {
+      title: 1, author: 1, url: 1, likes: 1,
+    });
   response.json(users);
 });
 
 usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body;
+  const {
+    username, name, password,
+  } = request.body;
 
   if (password.length < 3) {
     const errorMessage = 'User validation failed: Path `password` is shorter than the minimum allowed length (3).';
